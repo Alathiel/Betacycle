1eebcd061f67445ae22a4be469d63e03335270bf
MarkelosI <marcyburgio@gmail.com>
2024-05-16 17:40:15 +0200

    Frontend Frankestein Brutto in culo

---
+++ Frontend/Frontend Frankestein/BETACYCLE/src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig,importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoggedInterceptorService } from './shared/http-interceptors/logged-interceptor.interceptor';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes),
+    importProvidersFrom(HttpClientModule),
+    provideAnimationsAsync(),
+    { provide: HTTP_INTERCEPTORS, useClass: LoggedInterceptorService, multi: true }
+  ]
+};
---
+++ Frontend/Frontend Frankestein/BETACYCLE/src/app/core/registration/registration.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { RouterModule,Router,RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthServiceService } from '../../shared/services/auth-service.service';
+import { HttprequestservicesService } from '../../shared/services/httprequestservices.service';
+import { Credentials } from '../../shared/models/credential';
+import { User } from '../../shared/models/user';
+import { HttpStatusCode } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { loginservice } from '../../shared/services/loginservice.service';
+
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [CommonModule,FormsModule,MatCardModule],
+  templateUrl: './registration.component.html',
+  styleUrl: './registration.component.css'
+})
+export class RegistrationComponent {
+
+  credentials: Credentials = new Credentials();
+  disabled: boolean = false;
+  stayConnected: boolean = false;
+  newUser: User = new User();
+  newCredentials: Credentials = new Credentials();
+
+  constructor(private http:HttprequestservicesService, private logservice:loginservice,public router: Router, private AuthService:AuthServiceService){
+    
+  }
+
+  register(){
+    this.AuthService.registerUserData(this.newUser).subscribe({
+      next: (jsData: any) => {
+
+        this.newCredentials.userId = jsData.userId;
+
+        this.AuthService.registerCredentials(this.newCredentials).subscribe({
+          next: (jsData: any) =>{
+            this.router.navigate(['/login'])
+          },
+          error: (error) => {
+            console.log(error)
+            this.http.deleteUserData(this.newCredentials.userId).subscribe({
+              next: (jsData: any) =>{
+                console.log(jsData)
+              },
+              error: (error) => {}
+            })
+          }
+          
+        }) 
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+  }
+
+}
